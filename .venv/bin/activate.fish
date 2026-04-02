e2a8fb43cc69e224236b2245592f50d595be03d8
Kuroraiun <blackthundercloud24a001@gmail.com>
2026-04-02 14:31:27 +0900

    Update docs

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/kuroraiun/projects/python/mkdocs/.venv
+set -gx VIRTUAL_ENV /home/kuroraiun/projects/02-assistant/python/mkdocs/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
